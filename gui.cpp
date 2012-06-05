312a5534fc7acb538f6210da70773719286eced3
Mark Sinclair <125axel125@gmail.com>
2012-06-04 23:19:13 -0500

    Simple proof of concept gui. one button

---
+++ gui.cpp
@@ -0,0 +1,59 @@
+#include <gtkmm.h>
+#include <iostream>
+//~ #include <fstream>
+//~ #include <string>
+//~ #include <sstream>
+//~ #include <unistd.h>
+//~ #include <cstdlib>
+
+#include "life.hpp"
+#include "main.hpp"
+
+class MyWin : public Gtk::Window
+{
+public:
+	MyWin();
+	virtual ~MyWin();
+	
+protected:
+	/* Run program */
+	void on_go_clicked();
+	
+	/* Child thing */
+	Gtk::Button go_button;
+};
+
+MyWin::MyWin()
+{
+	go_button.add_pixlabel("info.xpm","Go!");
+	
+	set_title("Conway's Game of Life");
+	set_border_width(10);
+	
+	go_button.signal_clicked().connect(sigc::mem_fun(*this,
+			&MyWin::on_go_clicked));
+			
+	add(go_button);
+	
+	show_all_children();
+};
+
+void MyWin::on_go_clicked() {
+	process("samples/weekender.txt",20,20,20,true,true,true,false);
+}
+
+MyWin::~MyWin()
+{
+}
+
+int main(int argc, char *argv[])
+{
+	Glib::RefPtr<Gtk::Application> app =
+	Gtk::Application::create(argc, argv,
+		"org.gtkmm.examples.base");
+
+	//~ Gtk::Window window;
+	MyWin window;
+
+	return app->run(window);
+}
