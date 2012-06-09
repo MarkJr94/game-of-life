af5e64a5eae03eed0a95081aea28acad6a7d3bb0
Mark Sinclair <125axel125@gmail.com>
2012-06-08 23:46:04 -0500

    Added gui.hpp header file

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: life.cpp life.hpp main.cpp main.hpp cmd.cpp gui.cpp
+all: life.cpp life.hpp main.cpp main.hpp cmd.cpp gui.cpp gui.hpp
 	g++ -Wall -g cmd.cpp main.cpp life.cpp -o life-cmd
 	g++ -Wall -g gui.cpp main.cpp life.cpp -o life-gui `pkg-config --cflags --libs gtkmm-3.0`
 
@@ -6,7 +6,7 @@ all: life.cpp life.hpp main.cpp main.hpp cmd.cpp gui.cpp
 cmd: life.cpp life.hpp main.cpp main.hpp cmd.cpp
 	g++ -Wall -g cmd.cpp main.cpp life.cpp -o life-cmd
 
-gui: life.cpp life.hpp main.cpp main.hpp gui.cpp
+gui: life.cpp life.hpp main.cpp main.hpp gui.cpp gui.hpp
 	g++ -Wall -g gui.cpp main.cpp life.cpp -o life-gui `pkg-config --cflags --libs gtkmm-3.0`
 
 clean: 
--- gui.cpp
+++ gui.cpp
@@ -1,82 +1,17 @@
-#include <gtkmm/window.h>
-#include <gtkmm/button.h>
-#include <gtkmm/checkbutton.h>
-#include <gtkmm/box.h>
-#include <gtkmm/entry.h>
-#include <gtkmm/settings.h>
-#include <gtkmm/stock.h>
-//~ #include <gtkmm/stockitem.h>
-#include <gtkmm/stockid.h>
-#include <gtkmm/image.h>
-//~ #include <gtkmm.h>
 #include <iostream>
 
 #include "life.hpp"
 #include "main.hpp"
-
-class MyWin : public Gtk::Window
-{
-public:
-	MyWin();
-	virtual ~MyWin();
-	
-protected:
-	/* Run program */
-	void on_go_clicked();
-	void on_quit_clicked();
-	void on_auto_toggled();
-	
-	/* Eventually gonna be file choosing thing */
-	Gtk::Entry entry;
-	
-	/* Box and CheckButtons for wrapping horizontally
-	 * an vertically
-	 * */
-	Gtk::Box *wrapbox;
-	Gtk::CheckButton *check_wrapx, *check_wrapy;
-	Gtk::Box *vbox;
-	
-	/* Boxes for entering width, height, an choosing to find it
-	 * automatically
-	 * */
-	Gtk::Box *size_hbox;
-	Gtk::Box *size_ent_box;
-	Gtk::Box *size_auto_box;
-	
-	Gtk::CheckButton *auto_check;
-	Gtk::Entry enter_width, enter_height;
-	Gtk::Label width_label, height_label;
-	
-	/* Box for number of turns 
-	 * as well as entry */
-	 Gtk::Box *turn_box;
-	 Gtk::Label turn_label;
-	 Gtk::Entry enter_turns;
-	 
-	 /* Box for choice of printing
-	  * as well as pictorial or coordinate
-	  * file
-	  * */
-	Gtk::Box *choose_box;
-	Gtk::CheckButton *printing, *filetype;
-	
-	/* Primary buttons, go and quit 
-	 * and accompanying box
-	 * */
-	Gtk::Box *hbox;
-	Gtk::Button *go_button, *quit_bu;
-	
-	/* Helper function for putting stock labels on buttons */
-	Gtk::Button *set_but(const char *lab, const Gtk::StockID& stock);
-};
+#include "gui.hpp"
 
 MyWin::MyWin()
 {
 	using namespace Gtk;
 	set_title("Conway's Game of Life");
 	set_border_width(10);
-	set_default_size(200,200);
+	set_default_size(500,200);
 	vbox = manage(new Box(Gtk::ORIENTATION_VERTICAL));
+	add(*vbox);
 	
 	entry.set_text("/home/markjr/game-of-life/samples/puffsup");
 	vbox->pack_start(entry);
@@ -144,7 +79,7 @@ MyWin::MyWin()
 	hbox->pack_start(*quit_bu);
 				
 	vbox->pack_start(*hbox);
-	add(*vbox);
+	
 	
 	show_all_children();
 };
